04c4c90610772d5820574f88fc7e6d8b9ee9b06d
wuyiqun0718 <wuyiqun0718@gmail.com>
2017-12-07 23:10:31 -0600

    import the redux and added the reducer, store, actions for the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import calendar from './reducer';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    calendar,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
