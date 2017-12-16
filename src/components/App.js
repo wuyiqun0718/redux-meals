7b08e0520c0da290989ec9ac82eb0a496bb0ba33
wuyiqun0718 <wuyiqun0718@gmail.com>
2017-12-16 11:33:32 -0600

    import react-redux and initiate the connect method

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,27 @@
 import React, { Component } from 'react';
-import { addRecipe } from '../actions';
+import { connect } from 'react-redux';
 import '../App.css';
 
 class App extends Component {
-  state = {
-    calendar: null,
-  }
-  componentDidMount() {
-    const { store } = this.props;
-
-    store.subscribe( () => {
-      this.setState(() => {
-        return { calendar: store.getState() }
-      })
-    })
-  }
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }));
-    this.input.value = '';
-  }
   render() {
+    console.log('Props', this.props);
     return (
       <div>
-        <input 
-          type='text'
-          ref={input => this.input = input}
-        />
-        <button onClick={this.submitFood}>Submit</button>
-        <pre>Monday's breakfast is {this.state.calendar && this.state.calendar.monday.breakfast}</pre>
+        Hello World
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.assign({}, calendar[day])
+    })),
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import calendar from './reducer';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const store = createStore(
     calendar,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
